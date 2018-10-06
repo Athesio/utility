2765423ed14663af2f71bd4ae113b4350913f8ec
Taro yamashita <taro.c.yamashita@gmail.com>
2018-10-05 19:17:41 -0500

    implement mother and child server execution environment

--- motherServer/index.js
+++ motherServer/index.js
@@ -13,14 +13,14 @@ app.use(cors());
 
 let PORT = process.env.PORT || 3069; 
 
-
-
+const image = '9c27d1bad198'
 app.post('/', (req, res)=>{
+
     let portAss = Math.floor(Math.random()*5000);
     let code = req.body;
     let dockerPort = 3000+portAss;
     console.log('docker port is ', dockerPort);
-    exec(`docker run -p ${dockerPort.toString()}:3000 a45f97e1ad6c`, (err, stdout)=>{
+    exec(`docker run -p ${dockerPort.toString()}:3000 ${image}`, (err, stdout)=>{
         console.log('cb invoked');
         if(err) {
           console.log('error is', err);
@@ -32,12 +32,13 @@ app.post('/', (req, res)=>{
       });
       setTimeout(()=>{
         console.log('sending request');
-        axios.post(`http://127.0.0.1:3000`, `console.log('howdy from Mother')`, {headers: {
+        axios.post(`http://127.0.0.1:6192`, code, {headers: {
             'Content-Type': 'text/plain'
         }}).then((response)=>{
+            
              console.log(response);
             res.send(response.data);
-        }).catch((err)=>{res.send(err)})}, 1000);
+        }).catch((err)=>{res.send(err)})}, 2000);
     
       ;
 })
