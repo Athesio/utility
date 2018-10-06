5b5b82ddf075a4365a467557e25c88e76cfaef38
Taro yamashita <taro.c.yamashita@gmail.com>
2018-10-06 15:38:39 -0500

    update and clean up files

--- child-server/server/testFile.js
+++ child-server/server/testFile.js
@@ -1,1 +1,11 @@
-console.log('howdy from Mother')
+function getLengthOfWord(word) {
+  // your code here
+  /* START SOLUTION */
+  console.log(word.length);
+  return word.length;
+  
+  /* END SOLUTION */
+}
+
+getLengthOfWord('supercalifragilisticexpealidocious');
+getLengthOfWord('supercalifragilisticexpealidocioussupercalifragilisticexpealidocioussupercalifragilisticexpealidocious');
