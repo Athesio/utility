deeca343de91682239a6743159d4c082421384e6
Shawn Acevedo <sxa@Shawns-MacBook-Pro.local>
2018-10-10 19:36:21 -0500

    use spawn to trigger container

--- mother-server/index.js
+++ mother-server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('file-system');
 const bodyParser = require('body-parser');
-const {spawn, exec} = require('child_process');
+const { spawn, exec } = require('child_process');
 const cors = require('cors');
 const axios = require('axios');
 
@@ -11,46 +11,71 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.text());
 app.use(cors());
 
-let PORT = process.env.PORT || 3069; 
+let PORT = process.env.PORT || 3069;
 
 let awsImage = 'fa917a185517'
-let localImage = '9c27d1bad198'
+let localImage = '8343ca51e590'
 
 const image = localImage;
-app.post('/', (req, res)=>{
+app.post('/', (req, res) => {
 
-    let portAss = Math.floor(Math.random()*5000);
-    let code = req.body;
-    let dockerPort = 3000+portAss;
-    let containerName = 'container'+dockerPort.toString();
-    console.log('req is', req.body);
-    console.log('docker port is ', dockerPort);
-    exec(`docker run --name ${containerName} -p ${dockerPort.toString()}:3000 ${image}`, (err, stdout)=>{
-        console.log('cb invoked');
-        if(err) {
-          console.log('error is', err);
-        } else {
-          console.log('standard output is...', stdout);
-        }
+  let portAss = Math.floor(Math.random() * 5000);
+  let code = req.body;
+  let dockerPort = 3000 + portAss;
+  let containerName = 'container' + dockerPort.toString();
+  console.log('req is', req.body);
+  console.log('docker port is ', dockerPort);
+  // exec(`docker run --name ${containerName} -p ${dockerPort.toString()}:3000 ${image}`, (err, stdout) => {
+  //   console.log('cb invoked');
+  //   if (err) {
+  //     console.log('error is', err);
+  //   } else {
+  //     console.log('standard output is...', stdout);
+  //   }
 
-    let codedOutput = ""
-      });
-      setTimeout(()=>{
-        console.log('sending request');
-        axios.post(`http://127.0.0.1:${dockerPort.toString()}`, code, {headers: {
-            'Content-Type': 'text/plain'
-        }}).then((response)=>{
-            
-             console.log(response);
-            res.send(JSON.stringify(response.data));
-            exec(`docker stop ${containerName}`);
-        }).catch((err)=>{res.send(err)
-          exec(`docker stop ${containerName}`);
-        })}, 2000);
+  //   let codedOutput = ""
+  // });
+  // setTimeout(() => {
+  //   console.log('sending request');
+  //   axios.post(`http://127.0.0.1:${dockerPort.toString()}`, code, {
+  //     headers: {
+  //       'Content-Type': 'text/plain'
+  //     }
+  //   }).then((response) => {
+
+  //     console.log(response);
+  //     res.send(JSON.stringify(response.data));
+  //     exec(`docker stop ${containerName}`);
+  //   }).catch((err) => {
+  //     res.send(err)
+  //     exec(`docker stop ${containerName}`);
+  //   })
+  // }, 2000);
+  const child =  spawn(`docker run --name ${containerName} -p ${dockerPort.toString()}:3000 ${image}`, {
     
-      ;
-})
+    shell: true
+  });
+  child.stdout.once('data', (data) => {
+    console.log('child stdout',data.toString());
+    axios.post(`http://127.0.0.1:${dockerPort.toString()}`, code, {
+      headers: {
+        'Content-Type': 'text/plain'
+      }
+    }).then((response) => {
+
+      console.log(response);
+      res.send(JSON.stringify(response.data));
+      exec(`docker stop ${containerName}`);
+    }).catch((err) => {
+      res.send(err)
+      exec(`docker stop ${containerName}`);
+    })
+    
+  })
+
+
+});
 
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}!`);
-  });
+  console.log(`listening on port ${PORT}!`);
+});
