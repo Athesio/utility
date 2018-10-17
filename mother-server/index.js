c627de9c204091fcd3bb9c09656ca12149fb3106
Shawn Acevedo <sxa@Shawns-MacBook-Pro.local>
2018-10-17 16:00:21 -0500

    auto deletes container

--- mother-server/index.js
+++ mother-server/index.js
@@ -17,40 +17,17 @@ let awsImage = 'fa917a185517'
 let localImage = '8343ca51e590'
 
 const image = localImage;
+
+
 app.post('/', (req, res) => {
 
   let portAss = Math.floor(Math.random() * 5000);
   let code = req.body;
   let dockerPort = 3000 + portAss;
   let containerName = 'container' + dockerPort.toString();
-  console.log('req is', req.body);
+  
   console.log('docker port is ', dockerPort);
-  // exec(`docker run --name ${containerName} -p ${dockerPort.toString()}:3000 ${image}`, (err, stdout) => {
-  //   console.log('cb invoked');
-  //   if (err) {
-  //     console.log('error is', err);
-  //   } else {
-  //     console.log('standard output is...', stdout);
-  //   }
-
-  //   let codedOutput = ""
-  // });
-  // setTimeout(() => {
-  //   console.log('sending request');
-  //   axios.post(`http://127.0.0.1:${dockerPort.toString()}`, code, {
-  //     headers: {
-  //       'Content-Type': 'text/plain'
-  //     }
-  //   }).then((response) => {
-
-  //     console.log(response);
-  //     res.send(JSON.stringify(response.data));
-  //     exec(`docker stop ${containerName}`);
-  //   }).catch((err) => {
-  //     res.send(err)
-  //     exec(`docker stop ${containerName}`);
-  //   })
-  // }, 2000);
+  
   const child =  spawn(`docker run --name ${containerName} -p ${dockerPort.toString()}:3000 ${image}`, {
     
     shell: true
@@ -63,12 +40,18 @@ app.post('/', (req, res) => {
       }
     }).then((response) => {
 
-      console.log(response);
+      
       res.send(JSON.stringify(response.data));
       exec(`docker stop ${containerName}`);
+      setTimeout(()=>{
+        exec(`docker rm ${containerName}`)
+      }, 15000);
     }).catch((err) => {
       res.send(err)
       exec(`docker stop ${containerName}`);
+      setTimeout(()=>{
+        exec(`docker rm ${containerName}`)
+      }, 15000);
     })
     
   })
